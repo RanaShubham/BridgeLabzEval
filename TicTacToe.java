3154b5e071d22937f37a82d21c6f371d3855c0d4
Shubham Rana <ranashubham7296@gmail.com>
2020-10-08 10:35:17 +0530

    UC_2

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,16 @@
 package BridgeLabzEval;
 
-public class TicTacToe {
+import java.util.Scanner;
 
+public class TicTacToe {
+	
 	public static void main(String[] args) 
 	{
 		TicTacToe board = new TicTacToe();
 		board.createCheckBoard();
+		int player = board.chooseXorO();
+		int computer = 1 - player;
+		
 	}
 
 
@@ -13,5 +18,17 @@ public class TicTacToe {
 	public void createCheckBoard()
 	{
 		char [] checkBoard = new char [10];
+	}
+	
+	public int chooseXorO()
+	{
+		System.out.println("Enter y if you want to choose O, Enter n if you want to choose X");
+		Scanner s = new Scanner(System.in);
+		String choice = s.next();
+		
+		if (choice.equals("y"))
+			return 1;
+		else
+			return 0;
 	}
 }
