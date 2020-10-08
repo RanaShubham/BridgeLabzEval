1a0dd9c42800de8834102bcb8c81ae99664cd60f
Shubham Rana <ranashubham7296@gmail.com>
2020-10-08 10:02:07 +0530

    UC_1 Done

---
+++ UC_1.java
@@ -0,0 +1,16 @@
+package BridgeLabzEval;
+
+public class UC_1 {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+
+
+	public void createCharArr()
+	{
+		char [] charArr = new char [10];
+	}
+}
